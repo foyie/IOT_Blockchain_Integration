300be683b8b8e92cced94e02b828527dca55633d
Chandrima Das <89987028+foyie@users.noreply.github.com>
2023-10-07 02:00:23 +0530

    ngo_crowdfund.sol

--- lotter.sol
+++ lotter.sol
@@ -1,40 +1,80 @@
-// SPDX-License-Identifier: GPL-3.0
+//chandrima
+pragma solidity >=0.5.0 < 0.9.0;
 
-pragma solidity >=0.7.0 <0.9.0;
-
-contract Lottery{
- //entities - manager,players and winner
- address public manager;
- address payable[] public players;
- address payable public winner;
-
-  constructor(){
-      manager=msg.sender;
-  }
-
-  function participate() public payable{
-      require(msg.value==1 ether,"Please pay 1 ether only");
-      players.push(payable(msg.sender));
-  }
-
-  function getBalance() public view returns(uint){
-      require(manager==msg.sender,"You are not the manager");
-      return address(this).balance;
-  }
-
-  function random() internal view returns(uint){
-      return uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,players.length)));
-  }
-
-  function pickWinner() public{
-      require(manager==msg.sender,"You are not the manager");
-      require(players.length>=3,"Players are less than 3");
-
-      uint r=random();
-      uint index = r%players.length;
-      winner=players[index];
-      winner.transfer(getBalance());
-      players= new address payable[](0); //this will intiliaze the players array back to 0
-  }
-
+contract CrowdFunding{
+    mapping(address=>uint) public contributors; //contributors[msg.sender]=100
+    address public manager; 
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+    
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+    mapping(uint=>Request) public requests;
+    uint public numRequests;
+    constructor(uint _target,uint _deadline){
+        target=_target;
+        deadline=block.timestamp+_deadline; //10sec + 3600sec (60*60)
+        minimumContribution=100 wei;
+        manager=msg.sender;
+    }
+    
+    function contribute() public payable{
+        require(block.timestamp < deadline,"Deadline has passed");
+        require(msg.value >=minimumContribution,"Minimum Contribution is not met");
+        
+        if(contributors[msg.sender]==0){
+            noOfContributors++;
+        }
+        contributors[msg.sender]+=msg.value;
+        raisedAmount+=msg.value;
+    }
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    function refund() public{
+        require(block.timestamp>deadline && raisedAmount<target,"You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user=payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+        
+    }
+    modifier onlyManger(){
+        require(msg.sender==manager,"Only manager can calll this function");
+        _;
+    }
+    function createRequests(string memory _description,address payable _recipient,uint _value) public onlyManger{
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description=_description;
+        newRequest.recipient=_recipient;
+        newRequest.value=_value;
+        newRequest.completed=false;
+        newRequest.noOfVoters=0;
+    }
+    function voteRequest(uint _requestNo) public{
+        require(contributors[msg.sender]>0,"YOu must be contributor");
+        Request storage thisRequest=requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+    function makePayment(uint _requestNo) public onlyManger{
+        require(raisedAmount>=target);
+        Request storage thisRequest=requests[_requestNo];
+        require(thisRequest.completed==false,"The request has been completed");
+        require(thisRequest.noOfVoters > noOfContributors/2,"Majority does not support");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed=true;
+    }
 }
+
